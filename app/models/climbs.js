8f26bf015a9b3504f7abc140bdb175d6c9133409
gundyn <nathan.gundy@gmail.com>
2020-11-05 09:57:05 -0600

    climb Schema created

--- app/models/climbs.js
+++ app/models/climbs.js
@@ -1,3 +1,29 @@
 const mongoose = require('mongoose')
 
-const climbSchema = new mongoose.Schema({})
+const climbSchema = new mongoose.Schema({
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  rating: {
+    type: Number,
+    required: true
+  },
+  grade: {
+    type: Number,
+    required: true
+  },
+  reveiw: {
+    type: String,
+    required: true
+  }
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model('Climb', climbSchema)
